7f7eab15ec69f82bdf0160145c083906b1467c8d
Bob Schellink <sabob1@gmail.com>
2016-01-12 19:33:40 +0200

    docs

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
 # Welcome to Kudu Java Template
 
-This is a starting template for using Kudu in a Java project
+This is a starting template for using Kudu in a Java project.
+
+Checkout
+--------
+To checkout this project do the following:
+
+```
+git clone https://github.com/kudujs/java-template.git yourProjectName
+cd yourProjectName
+```
+
+This will checkout the project into a local folder called 'yourProjectName'
 
 Run
 ---
 You can run the template on __node express__ as follows:
-1. navigate to the _web_ folder, 
-2. run 'web>npm install' to download the node modules
-3. run 'web>node server' to start the express server and open a browser window at the template url
+
+```
+navigate to the 'web' folder
+run 'npm install' to download the node modules
+run 'node server' to start the express server and open a browser window at the template url
+```
+
+
+Build
+-----
+To build an optimized version of your Javascript application, perform the following steps:
+
+```
+cd 'jsbuild'
+npm install
+node build
+```
+
+"node build" will also create an optimized version of the examples and copy the output to the folder 'build/web/'.
--- jsbuild/build.js
+++ jsbuild/build.js
@@ -1,9 +1,3 @@
-/* modules: 
- * https://www.npmjs.com/package/fs-extra
- * https://www.npmjs.com/package/glob
- * https://www.npmjs.com/package/node-version-assets
- */
-
 var cli = require('./mods/cli');
 var config = require('./mods/config');
 var optimize = require('./mods/optimize');
@@ -12,65 +6,70 @@ var versioning = require("node-version-assets");
 var glob = require("glob");
 var copyDereferenceSync = require('copy-dereference').sync;
 
+build();
+
+function build() {
+
 // Read in the build config file
-var rConfig = fs.readFileSync("./config/r.build.js", 'utf8');
-rConfig = eval(rConfig);
-var appConfig = config.get("app");
+    var rConfig = fs.readFileSync("./config/r.build.js", 'utf8');
+    rConfig = eval(rConfig);
+    var appConfig = config.get("app");
 
 // Remove the deploy folder in case of previous builds
-clean(rConfig);
+    clean(rConfig);
 
-optimize(rConfig, appConfig).then(function (buildResponse) {
+    optimize(rConfig, appConfig).then(function (buildResponse) {
 
-	try {
-		renameConfigToRequire(rConfig);
-		copyExpressArtifact(rConfig);
+        try {
+            renameConfigToRequire(rConfig);
+            copyExpressArtifact(rConfig);
 
-		versionAssets(rConfig).then(function () {
-			console.log("Build completed successfully!");
-		});
-	} catch (e) {
-		console.error(e.stack);
-	}
-});
+            versionAssets(rConfig).then(function () {
+                console.log("Build completed successfully!");
+            });
+        } catch (e) {
+            console.error(e.stack);
+        }
+    });
 
-function versionAssets(rConfig) {
+    function versionAssets(rConfig) {
 
-	var version = new versioning({
-		assets: [rConfig.dir + '/css/site.css', rConfig.dir + '/js/lib/require.js'],
-		grepFiles: [rConfig.dir + '/index.jsp']
-	});
+        var version = new versioning({
+            assets: [rConfig.dir + '/css/site.css', rConfig.dir + '/js/lib/require.js'],
+            grepFiles: [rConfig.dir + '/index.jsp']
+        });
 
-	var promise = new Promise(function (resolve, reject) {
+        var promise = new Promise(function (resolve, reject) {
 
-		version.run(function () {
-			resolve();
-		});
-	});
+            version.run(function () {
+                resolve();
+            });
+        });
 
-	return promise;
+        return promise;
+    }
+
+    function copyExpressArtifact(rConfig) {
+        var source = "./artifacts";
+        var target = rConfig.dir;
+
+        fs.copySync(source, target);
+        console.log(source + " copied Node Express artifacts to " + target);
+    }
+
+    function renameConfigToRequire(rConfig) {
+        var source = rConfig.dir + "js/app/config/config.js";
+        var target = rConfig.dir + "js/lib/require.js";
+
+        fs.renameSync(source, target);
+        console.log(source + " renamed to " + target);
+    }
+
+    function clean(rConfig) {
+        fs.removeSync(rConfig.dir);
+        fs.removeSync("../../kudu-examples-pages/js");
+        console.log("Removed previous buildpath: " + rConfig.dir);
+    }
+
+    console.log("Running build in", config.environment(), "mode");
 }
-
-function copyExpressArtifact(rConfig) {
-	var source = "./artifacts";
-	var target = rConfig.dir;
-
-	fs.copySync(source, target);
-	console.log(source + " copied Node Express artifacts to " + target);
-}
-
-function renameConfigToRequire(rConfig) {
-	var source = rConfig.dir + "js/app/config/config.js";
-	var target = rConfig.dir + "js/lib/require.js";
-
-	fs.renameSync(source, target);
-	console.log(source + " renamed to " + target);
-}
-
-function clean(rConfig) {
-	fs.removeSync(rConfig.dir);
-	fs.removeSync("../../kudu-examples-pages/js");
-	console.log("Removed previous buildpath: " + rConfig.dir);
-}
-
-console.log("Running build in", config.environment(), "mode");
